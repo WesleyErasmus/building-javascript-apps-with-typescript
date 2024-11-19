4ed0cc3a8de0bff592f5d32706527110cbaeb6bb
Wesley Erasmus <wesley.erasmus119@gmail.com>
2024-11-19 18:20:30 +0200

    completed and uploading module 8. This completes the course

---
+++ code/module-08/m08-start/module08_loan-programs.ts
@@ -0,0 +1,34 @@
+/// <reference path="module08_loan-programs.ts">
+
+import { Loans } from "./module08_loans";
+/*  TODO Create LoanPrograms namespace. */
+
+export namespace LoanPrograms {
+  /*  TODO Update the calculateInterestOnlyLoanPayment function. */
+
+  export function calculateInterestOnlyLoanPayment(
+    loanTerms: Loans.Loan
+  ): string {
+    let payment: number;
+    payment =
+      loanTerms.principle * calculateInterestRate(loanTerms.interestRate);
+    return "The interest only loan payment is " + payment.toFixed(2);
+  }
+
+  /*  TODO Update the calculateConventionalLoanPayment function. */
+
+  export function calculateConventionalLoanPayment(
+    loanTerms: Loans.ConventionalLoan
+  ): string {
+    let interest: number = calculateInterestRate(loanTerms.interestRate);
+    let payment: number;
+    payment =
+      (loanTerms.principle * interest) /
+      (1 - Math.pow(1 / (1 + interest), loanTerms.months));
+    return "The conventional loan payment is " + payment.toFixed(2);
+  }
+  export function calculateInterestRate(interestRate: number): number {
+    let interest: number = interestRate / 1200;
+    return interest;
+  }
+}
